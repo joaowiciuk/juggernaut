081fbbd8b16c29c1d977a0428ee1e7159b6c63a0
joaowiciuk <joaowiciuk@hotmail.com>
2019-07-12 18:27:12 -0500

    Redes Wifi

--- adaptador_bluetooth.go
+++ adaptador_bluetooth.go
@@ -18,6 +18,7 @@ type adaptadorBluetooth struct {
 	registro    *os.File
 	registrador *log.Logger
 	device      gatt.Device
+	descSSIDS   bool
 }
 
 func newAdaptadorBluetooth() (a *adaptadorBluetooth) {
@@ -44,86 +45,103 @@ func (a *adaptadorBluetooth) desconexao() (f func(gatt.Central)) {
 
 func (a *adaptadorBluetooth) descobertaWifi() *gatt.Service {
 	s := gatt.NewService(gatt.UUID16(0x1815))
-	c := s.AddCharacteristic(gatt.UUID16(0x2A04))
-	c.HandleNotifyFunc(
-		func(r gatt.Request, notifier gatt.Notifier) {
-			//Enquanto as notificações não forem desativadas para a Characteristic...
-			for !notifier.Done() {
-				//Comando para verificar redes wifi disponíveis
-				cmd := exec.Command("/bin/sh", "-c", "sudo iw dev wlan0 scan | grep SSID")
+	caracSSIDs := s.AddCharacteristic(gatt.UUID16(0x2A04))
+	caracSSIDs.HandleNotifyFunc(func(r gatt.Request, notifier gatt.Notifier) {
+		//Enquanto as notificações não forem desativadas para a Characteristic...
+		for !notifier.Done() {
 
-				//Saída padrão do comando
-				stdout, err := cmd.StdoutPipe()
-				if err != nil {
-					a.registrador.Println(err)
-					return
-				}
-
-				//Inicia o comando porém não aguarda finalização
-				if err := cmd.Start(); err != nil {
-					a.registrador.Println(err)
-					return
-				}
-
-				//Converte a saída do comando para string
-				buf := new(bytes.Buffer)
-				buf.ReadFrom(stdout)
-				output := buf.String()
-
-				//Aguarda até que o comando finalize
-				if err := cmd.Wait(); err != nil {
-					a.registrador.Println(err)
-					return
-				}
-
-				//Expressão regular para identificar a informação desejada na saída do comando
-				re := regexp.MustCompile(`\ *SSID:\ (.*)`)
-				submatches := re.FindAllStringSubmatch(output, -1)
-				ssids := make([]string, 0)
-				ssidsSource := new(bytes.Buffer)
-
-				//Monta uma lista de ssids a partir da saída do comando
-				//Também armazena essa lista no ssidsSource, para uso futuro
-				for _, submatch := range submatches {
-					ssids = append(ssids, submatch[1])
-					io.WriteString(ssidsSource, submatch[1])
-				}
-				if len(ssids) < 2 {
-					a.registrador.Printf("error: no ssid")
-					return
-				}
-
-				//Registra todos os ssids encontrados
-				for _, ssid := range ssids {
-					a.registrador.Printf("%s\n", ssid)
-				}
-
-				//Buffer de transferência para enviar o ssidSource em pedaços de 8 bytes
-				bufferTransf := make([]byte, 8)
-
-				//Inicia a transferência de ssidSource por mensagens do notifier
-				// >> IMPORTANTE: para esta característica são permitidos apenas 8 bytes por mensagem <<
-				for {
-					k, err := ssidsSource.Read(bufferTransf)
-
-					//registra o buffer de transferência
-					a.registrador.Printf("k = %v err = %v bufferTransf = %v\n", k, err, bufferTransf)
-
-					//registra o buffer de transferência
-					a.registrador.Printf("bufferTransf[:k] = %q\n", bufferTransf[:k])
-
-					//envia o buffer de transferência pelo notifier
-					fmt.Fprintf(notifier, "%s", bufferTransf[:k])
-					if err == io.EOF {
-						break
-					}
-				}
-
-				//Aguarda 10 segundos até a próxima verificação
-				time.Sleep(time.Second * 10)
+			//Se a descoberta de SSIDs não tiver sido solicitada, abortar
+			if a.descSSIDS == false {
+				return
 			}
-		})
 
+			//Comando para verificar redes wifi disponíveis
+			cmd := exec.Command("/bin/sh", "-c", "sudo iw dev wlan0 scan | grep SSID")
+
+			//Saída padrão do comando
+			stdout, err := cmd.StdoutPipe()
+			if err != nil {
+				a.registrador.Println(err)
+				return
+			}
+
+			//Inicia o comando porém não aguarda finalização
+			if err := cmd.Start(); err != nil {
+				a.registrador.Println(err)
+				return
+			}
+
+			//Converte a saída do comando para string
+			buf := new(bytes.Buffer)
+			buf.ReadFrom(stdout)
+			output := buf.String()
+
+			//Aguarda até que o comando finalize
+			if err := cmd.Wait(); err != nil {
+				a.registrador.Println(err)
+				return
+			}
+
+			//Expressão regular para identificar a informação desejada na saída do comando
+			re := regexp.MustCompile(`\ *SSID:\ (.*)`)
+			submatches := re.FindAllStringSubmatch(output, -1)
+			ssids := make([]string, 0)
+			ssidsSource := new(bytes.Buffer)
+
+			//Monta uma lista de ssids a partir da saída do comando
+			//Também armazena essa lista no ssidsSource, para uso futuro
+			for _, submatch := range submatches {
+				ssids = append(ssids, submatch[1])
+				io.WriteString(ssidsSource, submatch[1])
+			}
+			if len(ssids) < 2 {
+				a.registrador.Printf("error: no ssid")
+				return
+			}
+
+			//Registra todos os ssids encontrados
+			for _, ssid := range ssids {
+				a.registrador.Printf("%s\n", ssid)
+			}
+
+			//Buffer de transferência para enviar o ssidSource em pedaços de 8 bytes
+			bufferTransf := make([]byte, 8)
+
+			//Inicia a transferência de ssidSource por mensagens do notifier
+			// >> IMPORTANTE: para esta característica são permitidos apenas 8 bytes por mensagem <<
+			for {
+				k, err := ssidsSource.Read(bufferTransf)
+
+				//registra o buffer de transferência
+				a.registrador.Printf("k = %v err = %v bufferTransf = %v\n", k, err, bufferTransf)
+
+				//registra o buffer de transferência
+				a.registrador.Printf("bufferTransf[:k] = %q\n", bufferTransf[:k])
+
+				//envia o buffer de transferência pelo notifier
+				fmt.Fprintf(notifier, "%s", bufferTransf[:k])
+				if err == io.EOF {
+					break
+				}
+			}
+
+			//Aguarda 10 segundos até a próxima verificação, caso seja solicitada
+			time.Sleep(time.Second * 10)
+			a.descSSIDS = false
+		}
+	})
+	caracSSIDs.AddDescriptor(gatt.UUID16(0x2904)).SetValue([]byte("Base 64 encoded"))
+	caracSSIDs.AddDescriptor(gatt.UUID16(0x2901)).SetValue([]byte("Available ssids for connection"))
+
+	caracSolicitarDesc := s.AddCharacteristic(gatt.UUID16(0x2A04))
+	caracSolicitarDesc.HandleWriteFunc(func(r gatt.Request, data []byte) (status byte) {
+		if string(data) == "sim" {
+			a.descSSIDS = true
+		}
+		return gatt.StatusSuccess
+	})
+	caracSSIDs.AddDescriptor(gatt.UUID16(0x2901)).SetValue([]byte("Request ssids for connection"))
+	caracSSIDs.AddDescriptor(gatt.UUID16(0x2906)).SetValue([]byte("\"yes\" or \"no\""))
 	return s
 }
 
