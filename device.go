50789aadd66e00017384b6e55d5310dbadc2f9dc
joaowiciuk <joaowiciuk@hotmail.com>
2019-07-20 18:37:05 -0500

    Telemetria

--- device.go
+++ device.go
@@ -3,8 +3,8 @@ package main
 import "time"
 
 type Device struct {
-	UUID        string
-	Identifier  string
-	Temperature float64
-	LastUpdate  time.Time
+	UUID        string    `json:"uuid"`
+	Identifier  string    `json:"identifier"`
+	Temperature float64   `json:"temperature"`
+	LastUpdate  time.Time `json:"last_update"`
 }
