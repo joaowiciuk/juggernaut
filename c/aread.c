2e0b85ac6c9192034a8f0daf9ca8dfc543380d96
joaowiciuk <joaowiciuk@hotmail.com>
2019-08-15 21:19:05 -0500

    Running variance

--- c/aread.c
+++ c/aread.c
@@ -26,12 +26,15 @@ int main(void) {
 	int i;
 	float s2, sum, sum_of_sqr, cte;
 	for (i = 0; i < N; i++) {
-		//printf("%.2f\n", readVoltage(0));
+		printf("%.2f\n", readVoltage(0));
 		sum += readVoltage(0);
 		sum_of_sqr += pow(readVoltage(0), 2); 
 	}
 	cte = 1 / ((float) N * ((float) N - 1));
 	s2 = cte * ((float) N * sum_of_sqr - pow(sum, 2));
-	printf("%.2f\n", s2);
+	printf("cte: %.2f\n", cte);
+	printf("sum: %.2f\n", sum);
+	printf("sum_of_sqr: %.2f\n", sum_of_sqr);
+	printf("variance: %.2f\n", s2);
 	return EXIT_SUCCESS;
 }
