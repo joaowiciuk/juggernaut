d947b4f7610bf366bb9836b3e47ff02da4ac2409
joaowiciuk <joaowiciuk@hotmail.com>
2019-08-15 21:13:46 -0500

    Analog read

--- c/aread.c
+++ c/aread.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <unistd.h>
 #include <linux/i2c-dev.h>
 #include <sys/ioctl.h>
@@ -15,14 +16,22 @@
 
 #include "ads1115_rpi.h"
 
+#define N 5
+
 int main(void) {
 	if (openI2CBus("/dev/i2c-1") == -1) {
 		return EXIT_FAILURE;
 	}
 	setI2CSlave(0x48);
 	int i;
-	for (i = 0; i < 5; i++) {
-		printf("%.2f\n", readVoltage(0));
+	float s2, sum, sum_of_sqr, cte;
+	for (i = 0; i < N; i++) {
+		//printf("%.2f\n", readVoltage(0));
+		sum += readVoltage(0);
+		sum_of_sqr += pow(readVoltage(0), 2);
 	}
+	cte = 1 / ((float) N * ((float) N - 1))
+	s2 = cte * ((float) N * sum_of_sqr - pow(sum, 2))
+	printf("%.2f\n", s2);
 	return EXIT_SUCCESS;
 }
