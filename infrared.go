7b6b5dd908958ce62f42bcaddc09efe03daadfb1
joaowiciuk <joaowiciuk@hotmail.com>
2019-08-21 20:58:10 -0500

    Calling C function

--- infrared.go
+++ infrared.go
@@ -1,9 +1,8 @@
 package main
 
-// #cgo CFLAGS: -g -Wall
-// #cgo LDFLAGS: -L${SRCDIR}/c
+// #cgo CFLAGS: -g -Wall -I${SRCDIR}/c
 // #include <stdlib.h>
-// #include "./c/infrared.h"
+// #include "infrared.h"
 import "C"
 
 import (
