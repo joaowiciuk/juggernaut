46ea0622423dfd95f623dd0c2a9ede240781e755
joaowiciuk <joaowiciuk@hotmail.com>
2019-08-21 20:08:18 -0500

    Calling C function

--- c/infrared.c
+++ c/infrared.c
@@ -4,3 +4,11 @@
 int send(int pin, unsigned int signal) {
     return 10;
 }
+
+int greet(const char *name, int year, char *out) {
+    int n;
+    
+    n = sprintf(out, "Greetings, %s from %d! We come in peace :)", name, year);
+
+    return n;
+}
--- c/infrared.h
+++ c/infrared.h
@@ -2,5 +2,6 @@
 #define _INFRARED_H
 
 int send(int pin, unsigned int signal);
+int greet(const char *name, int year, char *out);
 
 #endif
